e6d9cfd8c060e2be246d463e012568de763abfb0
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-10 00:56:17 +0700

    Update Latest

--- configs/Database.js
+++ configs/Database.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 //สร้างฟังก์ชั่น 
 const connect = async () => {
     try {
-        // await mongoose.connect("mongodb://localhost:27017/Servers");
-        await mongoose.connect("mongodb+srv://Admin:1234@webhealth.kub1bgk.mongodb.net/?retryWrites=true&w=majority&appName=WebHealth/WebHealth");
+        await mongoose.connect("mongodb://localhost:27017/Servers");
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log(`Error connecting to MongoDB ${error}`);
