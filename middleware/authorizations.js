f31dad342fb7cac06e4e7b13bc896bd857b2bcf3
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-10 22:29:19 +0700

    Update Auth

---
+++ middleware/authorizations.js
@@ -0,0 +1,67 @@
+const { verifyJWT } = require("../configs/JWT");
+
+const authorized = (req, res, next) => {
+  const authorizations = req.headers.authorization;
+
+  if (authorizations == undefined) {
+    return res.status(400).json({ message: "เกิดข้อผิดพลาดกรุณาลองภายหลัง" });
+  }
+
+  const type = authorizations.split(" ")[0]; //Bearer
+  const token = authorizations.split(" ")[1]; //Bearer0
+
+  if (type != "Bearer" || token == undefined) {
+    return res.status(401).json({ message: "กรุณาเข้าระบบก่อนใช้งาน" });
+  }
+
+  const decodedToken = verifyJWT(token);
+
+  if (decodedToken == undefined) {
+    return res
+      .status(403)
+      .json({ message: "ท่านไม่ได้รับอนุญาติให้เข้าถึงข้อมูล" });
+  }
+
+  req.principal = decodedToken.principal;
+
+  next();
+};
+
+const isAdmin = (req, res, next) => {
+  const authorizations = req.headers.authorization;
+
+  if (authorizations == undefined) {
+    return res.status(400).json({ message: "เกิดข้อผิดพลาดกรุณาลองภายหลัง" });
+  }
+
+  const type = authorizations.split(" ")[0]; //Bearer
+  const token = authorizations.split(" ")[1]; //Bearer0
+
+  if (type != "Bearer" || token == undefined) {
+    return res.status(401).json({ message: "กรุณาเข้าระบบก่อนใช้งาน" });
+  }
+
+  const decodedToken = verifyJWT(token);
+
+  if (decodedToken == undefined) {
+    return res
+      .status(403)
+      .json({ message: "ท่านไม่ได้รับอนุญาติให้เข้าถึงข้อมูล" });
+  }
+
+  if (decodedToken.status != "admins") {
+    return res.status(403).json({
+      message: "ท่านไม่ได้รับอนุญาติให้เข้าถึงข้อมูล",
+      actual: { message: "เฉพาะเจ้าหน้าที่" },
+    });
+  }
+
+  req.principal = decodedToken.principal;
+
+  next();
+};
+
+module.exports = {
+  authorized,
+  isAdmin,
+};
--- schemas/users.js
+++ schemas/users.js
@@ -1,26 +1,25 @@
 const { Schema, model } = require("mongoose");
 const userSchema = new Schema(
-    {
-        _id:{
-            type: String,
-            required: true
-        },
-        username:{
-            type: String,
-            required: true
-        },
-        password:{
-            type: String,
-            required: true
-        },
-        status:{
-            type: String,
-            required: true,
-            // Default '2' == Users
-            default: "2"
-        },
+  {
+    _id: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "users",
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = model("users", userSchema);
